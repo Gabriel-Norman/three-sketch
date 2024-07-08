08120ce71cde10153b5cfbb0c420387799ccaa8a
Gabriel-Norman <42866338+Gabriel-Norman@users.noreply.github.com>
2024-07-08 12:55:37 +0200

    fix: adjust damp return

--- src/js/utils/math.js
+++ src/js/utils/math.js
@@ -2,7 +2,7 @@ const PI2 = Math.PI * 2.0;
 
 const lerp = (start, end, amt) => (1 - amt) * start + amt * end
 const invLerp = (x, y, a) => clamp((a - x) / (y - x))
-const damp = (start, end, amt, dt = 1) => { lerp(start, end, 1 - Math.exp(Math.log(1 - amt) * dt))}
+const damp = (start, end, amt, dt = 1) => lerp(start, end, 1 - Math.exp(Math.log(1 - amt) * dt))
 const round = (t) => Math.round(t * 100) / 100
 
 /**
